46a718eb043d323f9735c5dd31a85afab41a6ba2
Dzianis Karpiuk <dzianiskarpiuk@gmail.com>
2024-10-29 10:25:27 +0300

    add binary searchj

---
+++ Kobezza/Alghoritms/binarySearch.js
@@ -0,0 +1,28 @@
+//! Написать бинарный поиск!!!
+
+const binarySearch = (array, searchItem) => {
+	let start = 0
+	let end = array.length - 1
+	let currentIndex
+
+	while (start <= end) {
+		currentIndex = Math.floor((start + end) / 2)
+		const currentElement = array[currentIndex]
+
+		if (currentElement === searchItem) {
+			return currentIndex
+		}
+
+		if (currentElement > searchItem) {
+			end = currentIndex - 1
+		}
+
+		if (currentElement < searchItem) {
+			start = currentIndex + 1
+		}
+	}
+
+	return -1
+}
+
+console.log(binarySearch([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11], 8))
