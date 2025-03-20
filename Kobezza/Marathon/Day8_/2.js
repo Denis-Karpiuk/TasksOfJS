2e4f93a319255489c0476e3c627f1f412e835b30
Dzianis Karpiuk <dzianiskarpiuk@gmail.com>
2025-03-20 09:58:55 +0300

    add day 8 kobezza

--- Kobezza/Marathon/Day8_/2.js
+++ Kobezza/Marathon/Day8_/2.js
@@ -1,6 +1,55 @@
 // ## Написать функцию для сравнения двух объектов
-
 // Необходимо написать функцию для глубокого сравнения двух заданных объектов.
 
 console.log(compare({ a: 1, b: [1, 2, 3] }, { a: 1, b: [1, 2, 3] })) // true
 console.log(compare({ a: 1, b: [1, 2] }, { a: 1, b: [1, 2, 3] })) // false
+
+function compare(a, b) {
+	if (!isDeepCompare(a) && !isDeepCompare(b)) {
+		return a === b
+	}
+
+	if (a.constructor !== b.constructor) {
+		return false
+	}
+
+	if (Array.isArray(a)) {
+		if (a.length !== b.length) {
+			return false
+		}
+
+		for (let i = 0; i < a.length; i++) {
+			if (!compare(a[i], b[i])) {
+				return false
+			}
+		}
+		return true
+	}
+
+	const aEntries = Object.entries(a)
+	const bEntries = Object.entries(b)
+
+	if (aEntries.length !== bEntries.length) {
+		return false
+	}
+
+	aEntries.sort(sort)
+	bEntries.sort(sort)
+
+	for (let i = 0; i < aEntries.length; i++) {
+		if (!compare(aEntries[i], bEntries[i])) {
+			return false
+		}
+	}
+
+	return true
+
+	function sort([aKey], [bKey]) {
+		return aKey.localeCompare(bKey)
+	}
+
+	function isDeepCompare(value) {
+		const constr = value?.constructor
+		return Array.isArray(value) || constr === Object || constr == null
+	}
+}
--- Kobezza/Marathon/Day8_/3.js
+++ Kobezza/Marathon/Day8_/3.js
@@ -4,3 +4,24 @@
 // Элементами этого массива должны быть строки, для которых не существует анаграмм среди элементов первого массива.
 
 console.log(getUniqueStrs(['atoe', 'otea', 'ben', 'enb', 'baz', 'foo'])) // ['baz', 'foo']
+
+function getUniqueStrs(strs) {
+	const counter = new Map()
+
+	for (let str of strs) {
+		const serializeStr = [...str].sort().join('')
+
+		if (!counter.has(serializeStr)) {
+			counter.set(serializeStr, { count: 1, str })
+		} else {
+			counter.get(serializeStr).count++
+		}
+	}
+
+	return [...counter.values()].reduce((acc, { count, str }) => {
+		if (count === 1) {
+			acc.push(str)
+		}
+		return acc
+	}, [])
+}
