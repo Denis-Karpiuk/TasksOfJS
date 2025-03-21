cdf5fe8330803c522db050390af7f80f355d7c04
Dzianis Karpiuk <dzianiskarpiuk@gmail.com>
2025-03-22 01:00:11 +0300

    delete comment line

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ let str2 = 'тест'
 //! Базовый вариант
 function isPolindrom(string) {
 	let arr = string.split('') // разбиваем строку на массив символов
-	// add comment from first branch
+
 	// add comment from second branch
 	let arrReverse = arr.reverse() // перерворачиваем массив
 	// добавлен комментарий в ветку one
