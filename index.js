311df25046c63d0b5e97a65eefb859ed3c00995e
Dzianis Karpiuk <dzianiskarpiuk@gmail.com>
2025-03-23 13:37:55 +0300

    add test comment

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ function isPolindrom(string) {
 	let arr = string.split('') // разбиваем строку на массив символов
 	// let arr = string.split('') // разбиваем строку на массив символов
 
+	// добавлен комментарий в ветку one
+
 	let arrReverse = arr.reverse() // перерворачиваем массив
 	// добавлен комментарий в ветку one
 	let newString = arrReverse.join('') // склеиваем в новую строку
